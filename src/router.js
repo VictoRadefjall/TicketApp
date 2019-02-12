154547c255392f416ff7bceafebdfd890cc80941
Emil Petersson <emilpet93@gmail.com>
2019-02-12 13:34:53 +0100

    Created user pages and routes

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Buy from './views/Buy.vue'
+import Tickets from './views/Tickets.vue'
+import Events from './views/Events.vue'
 
 Vue.use(Router)
 
@@ -12,12 +15,19 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      path: '/buy',
+      name: 'buy',
+      component: Buy
+    },
+    {
+      path: '/tickets',
+      name: 'tickets',
+      component: Tickets
+    },
+    {
+      path: '/events',
+      name: 'events',
+      component: Events
+    },
   ]
 })
---
+++ src/views/Events.vue
@@ -0,0 +1,14 @@
+<template>
+
+</template>
+
+<script>
+
+export default {
+  name: 'events',
+}
+</script>
+
+<style>
+
+</style>
